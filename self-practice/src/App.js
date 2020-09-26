347b71eba4badaad9785f56ff9b0ee3d102bb11e
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-09-25 22:25:52 -0700

    Home Page Created

---
+++ self-practice/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import {
+  Home
+} from './Component/MainComponent/Home';
+
+function App() {
+  return ( <
+    Home / >
+  );
+}
+
+export default App;
---
+++ self-practice/src/Component/MainComponent/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Home = () => {
+  return <p> This is Home page</p>;
+};
